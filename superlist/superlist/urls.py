49abca93ee85fde01a872e57f9f668012dc09eeb
goohooh <goo_oo@naver.com>
2016-05-30 10:59:39 +0900

    Basic view return minimum HTML

--- superlist/superlist/urls.py
+++ superlist/superlist/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from lists.views import home_page
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^$', home_page, name='home'),
 ]
