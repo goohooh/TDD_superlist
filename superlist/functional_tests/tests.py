05e711f0951a253f03c0a4386d06f3d81e62c396
goohooh <goo_oo@naver.com>
2016-06-27 22:25:01 +0900

    스테이징 테스트를 위한 FT실행자 해킹

--- superlist/functional_tests/tests.py
+++ superlist/functional_tests/tests.py
@@ -1,6 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 import os
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -8,6 +9,20 @@ from selenium.webdriver.common.keys import Keys
 chromedriver = '/Users/seongpil/Applications/chromedriver'
 
 class NewVisitorTest(StaticLiveServerTestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
  
     def setUp(self):
         os.environ["webdriver.chrome.driver"] = chromedriver
